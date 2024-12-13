dff8a735e9b4ab4e59c5731602b496e1fc549867
aunalitahir <aunalitahir258@gmail.com>
2024-12-13 23:35:06 +0500

    js file 1

---
+++ script.js
@@ -0,0 +1,107 @@
+let doorOpen = prompt("Is the door open? (yes or no):");
+   
+    if (doorOpen === "yes") {
+        console.log("Alarm Triggered");
+    } else if (doorOpen === "no") {
+        console.log("All Safe");
+    } else {
+        console.log("Invalid input. Please answer with 'yes' or 'no'.");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let password = prompt("Enter your password:");
+
+// Check the length of the password
+if (password.length >= 8) {
+    console.log("Strong Password");
+} else if (password.length >= 5 && password.length <= 7) {
+    console.log("Moderate Password");
+} else {
+    console.log("Weak Password");
+}
+
+
+
+
+/*let weather =prompt ("Enter the weather condition(Sunny,Rainy Cold):").toLowerCase();
+if (weather =="Sunny"){
+    console.log("Wear a sun glasses");
+}
+else if (weather =="Rainy"){
+    console.log("take an umbrella");
+}
+else if (weather =="Cold"){
+    console.log("wear a jacket");
+}
+
+
+
+
+let temperature = parseFloat(prompt("Enter the water temperture in Celsius:"));
+if (temperature <=0){
+    console.log("Freezing");
+}
+else if (temperature >0 && temperature<=15){
+    console.log("cold");
+}
+else if (temperature >15 && temperature<=30){
+    console.log("warm");
+}
+else if (temperature >30){
+    console.log("Freezing")
+}
+
+
+
+let color = prompt("Enter a traffic light color (Red, Yellow, Green):");
+if (color === "red") {
+    console.log("Stop");
+} else if (color=== "yellow") {
+    console.log("Slow Down");
+} else if (color === "green") {
+    console.log("Go");
+} else {
+    console.log("Invalid Color");
+}
+
+
+
+
+
+
+ var userInput = prompt("enter 1 to turn ON the light or 0 to turn OFF the light");
+if (userInput ==0){
+    console.log("The light is OFF");
+}
+else if (userInput ==1){
+    console.log("The light is ON");
+}
+else{
+    console.log("invalid input")
+}*/
+
+
+
+
+
+
+
+
+
